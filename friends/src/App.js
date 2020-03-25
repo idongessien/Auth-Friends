3bd32eea9dc8b449c9861b3e1f9bb885db8bd178
Idong Essien <idong.essien@comcast.net>
2020-03-25 11:52:49 -0400

    Commit One

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,25 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+import PrivateRoute from "./components/PrivateRoute";
+
+import Login from "./components/Login";
+import Friends from "./components/Friends";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <ul>
+          <li>
+            <Link className= "nav-btn" to="/login">Login</Link>
+          </li>
+          <li>
+            <Link className= "nav-btn" to="/friends">Friends</Link>
+          </li>
+        </ul>
+        <Switch>
+          <PrivateRoute exact path="/friends" component={Friends} />
+          <Route path="/login" component={Login} />
+          <Route component={Login} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
